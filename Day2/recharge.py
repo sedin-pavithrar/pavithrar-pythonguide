e6740ac7953691d8ec531c25b7bd99b5fabffbe4
sedin-pavithrar <pavithra.r@sedintechnologies.com>
2026-06-10 12:54:42 +0530

    Recharge plan

---
+++ Day2/recharge.py
@@ -0,0 +1,11 @@
+# Mobile Recharge Plan Selector: 
+# Ask user for their budget and usage preference (calls / data / both). Recommend the best recharge plan from 3 options
+#  and show what they save vs the next plan. 
+
+# Enter your budget (₹): 300 
+# Preference (calls/data/both): data 
+# --- 
+# Best Plan: ₹239 — 1.5GB/day, 28 days 
+# You save ₹61 vs the ₹299 plan. 
+# Extra data vs ₹199 plan: +0.5GB/day 
+
