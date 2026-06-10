91af4614c1603f49865716dda3c7edfc49418bb2
sedin-pavithrar <pavithra.r@sedintechnologies.com>
2026-06-10 12:54:21 +0530

    Delivery Estimator

---
+++ Day2/delivery_estimator.py
@@ -0,0 +1,9 @@
+# Delivery Time Estimator: 
+# Take distance (km), weather (clear/rain/storm), and time of day (peak/normal) as input. 
+# Calculate estimated delivery time and show a status message like Zomato does. 
+# Distance: 4.5 km 
+# Weather (clear/rain/storm): rain 
+# Time (peak/normal): peak 
+# Estimated delivery: 42 mins 
+# Status: "Running slightly late due to rain" 
+
